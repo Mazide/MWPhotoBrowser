7811c30723aed16d9ab3885f567683c3df3ae9f2
n.demidov <nikdemm@gmail.com>
2017-05-02 17:57:41 +0300

    request title for 1 photos

--- Pod/Classes/MWPhotoBrowser.m
+++ Pod/Classes/MWPhotoBrowser.m
@@ -1093,13 +1093,13 @@ static void * MWVideoPlayerObservation = &MWVideoPlayerObservation;
             }
             self.title = [NSString stringWithFormat:@"%lu %@", (unsigned long)numberOfPhotos, photosText];
         }
-    } else if (numberOfPhotos > 1) {
+    } else {
         if ([_delegate respondsToSelector:@selector(photoBrowser:titleForPhotoAtIndex:)]) {
             self.title = [_delegate photoBrowser:self titleForPhotoAtIndex:_currentPageIndex];
         } else {
             self.title = [NSString stringWithFormat:@"%lu %@ %lu", (unsigned long)(_currentPageIndex+1), NSLocalizedString(@"of", @"Used in the context: 'Showing 1 of 3 items'"), (unsigned long)numberOfPhotos];
         }
-	} 
+	}
 	
 	// Buttons
 	_previousButton.enabled = (_currentPageIndex > 0);
